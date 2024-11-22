5126fa7e5d333576e1baacb0516f45f0d01f1403
Taylor Tu <taylor.tu@streetcontxt.com>
2024-11-22 11:53:26 -0500

    add demo code

---
+++ Advanced SiS Demo/sis_setup/create_app.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE STREAMLIT HACKATHON_GLACIAL_RIDERS.PUBLIC.cortex_analyst_demo_app
+ROOT_LOCATION = '@hackathon_glacial_riders.public.app_code'
+MAIN_FILE = 'Talk_to_your_data.py'
+QUERY_WAREHOUSE = RND_EXPLORATION_WH
+TITLE = 'Glacial Riders app';
